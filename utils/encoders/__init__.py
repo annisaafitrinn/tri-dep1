bafef4a7c9308f04a07db40bdb137f795d4c7f11
helemanc <elemanc@hotmail.it>
2026-03-22 22:53:06 +0100

    remove utils/encoders — encoders live in lib/models/encoders

--- utils/encoders/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Re-exports from lib.models.encoders (encoders live there now)."""
-
-from lib.models.encoders import (  # noqa: F401
-    AudioCNNEncoder,
-    AudioTemporalBiLSTMEncoder,
-    BiGRUAttentionEncoder,
-    CNNGRUEncoder,
-    CNNLSTMEncoder,
-)
